29b801c1c91947b1fb2de9455d3da0f8652b3959
alexbryw <alexbryw@gmail.com>
2021-01-21 17:27:32 +0100

    Added Sine and cos bars to Unit Circle.

--- components/Rect.js
+++ components/Rect.js
@@ -3,16 +3,24 @@ import { StyleSheet, Text, View } from 'react-native'
 import { useFrame } from 'react-three-fiber'
 
 const Rect = () => {
-    const group = useRef()
+    const rotor = useRef()
+    const sineMeter = useRef()
+    const cosMeter = useRef()
 
     useFrame(() => {
-        if(group && group.current) {
-            group.current.rotation.z += 0.03
+        if(rotor && rotor.current) {
+            rotor.current.rotation.z += 0.03
         }
+        sineMeter.current.scale.y = Math.sin(rotor.current.rotation.z)
+        sineMeter.current.position.y = Math.sin(rotor.current.rotation.z) / 2
+
+        cosMeter.current.scale.x = Math.cos(rotor.current.rotation.z)
+        cosMeter.current.position.x = Math.cos(rotor.current.rotation.z) / 2
+        
     })
     return (
     <>
-        <group ref={group} rotation={[0,0,THREE.Math.degToRad(0)]}>
+        <group ref={rotor} rotation={[0,0,THREE.Math.degToRad(0)]}>
             <mesh position={[0.5,0,0]} >
                 <boxBufferGeometry attach="geometry" args={[1,0.1,1]}/>
                 <meshStandardMaterial attach="material" color="orange"/>
@@ -25,6 +33,14 @@ const Rect = () => {
         <mesh>
             <ringBufferGeometry attach="geometry" args={[1.1, 1.2, 32, 32]} />
             <meshStandardMaterial attach="material" color="orange"/>
+        </mesh>
+        <mesh ref={sineMeter} position={[1.3,0,0]} >
+            <boxBufferGeometry attach="geometry" args={[0.1,1,0.1]}/>
+            <meshStandardMaterial attach="material" color="blue"/>
+        </mesh>
+        <mesh ref={cosMeter} position={[0,-1.3,0]} >
+            <boxBufferGeometry attach="geometry" args={[1,0.1,0.1]}/>
+            <meshStandardMaterial attach="material" color="red"/>
         </mesh>
     </>
     )
