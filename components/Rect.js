51a61f8d441ea2c8ba712f7b5a5531d7b6ac6d4b
alexbryw <alexbryw@gmail.com>
2021-01-24 22:01:38 +0100

    Moved animation outside of useFrame.

--- components/Rect.js
+++ components/Rect.js
@@ -11,59 +11,65 @@ const textOptions = {
     height: 0.02
   };
 
-const Rect = ({ rotate }) => {
-    const rotor = useRef()
-    const sineMeter = useRef()
-    const cosMeter = useRef()
+const Rect = ({ rotate = 0 }) => {
+    // const rotor = useRef()
+    // const sineMeter = useRef()
+    // const cosMeter = useRef()
 
 
     useFrame(() => {
-        // console.log(rotate)
-        if(rotor && rotor.current) {
-            // rotor.current.rotation.z += 0.03
-            rotor.current.rotation.z = rotate
+        // // console.log(rotate)
+        // if(rotor && rotor.current) {
+        //     // rotor.current.rotation.z += 0.03
+        //     rotor.current.rotation.z = rotate
 
-        }
-        sineMeter.current.scale.y = Math.sin(rotor.current.rotation.z)
-        sineMeter.current.position.y = Math.sin(rotor.current.rotation.z) / 2
+        // }
+        // sineMeter.current.scale.y = Math.sin(rotor.current.rotation.z)
+        // sineMeter.current.position.y = Math.sin(rotor.current.rotation.z) / 2
 
-        cosMeter.current.scale.x = Math.cos(rotor.current.rotation.z)
-        cosMeter.current.position.x = Math.cos(rotor.current.rotation.z) / 2
+        // cosMeter.current.scale.x = Math.cos(rotor.current.rotation.z)
+        // cosMeter.current.position.x = Math.cos(rotor.current.rotation.z) / 2
         
     })
+
     return (
-    <>
-        <group ref={rotor} rotation={[0,0,THREE.Math.degToRad(0)]}>
-            <mesh position={[0.5,0,0]} >
-                <boxBufferGeometry attach="geometry" args={[1,0.1,1]}/>
+        <>
+            /*center offset rotor. */
+            <group /*ref={rotor}*/ rotation={[0,0,rotate]}>
+                <mesh position={[0.5,0,0]} >
+                    <boxBufferGeometry attach="geometry" args={[1,0.1,1]}/>
+                    <meshStandardMaterial attach="material" color="orange"/>
+                </mesh>
+            </group>
+            <mesh>
+                <ringBufferGeometry attach="geometry" args={[1.1, 1.2, 32, 32]} />
                 <meshStandardMaterial attach="material" color="orange"/>
             </mesh>
-            {/* <mesh position={[-1,0,0]} rotation={[0,0,THREE.Math.degToRad(0)]}>
-                <boxBufferGeometry attach="geometry" args={[1,0.1,1]}/>
-                <meshStandardMaterial attach="material" color="orange"/>
-            </mesh> */}
-        </group>
-        <mesh>
-            <ringBufferGeometry attach="geometry" args={[1.1, 1.2, 32, 32]} />
-            <meshStandardMaterial attach="material" color="orange"/>
-        </mesh>
-        <mesh ref={sineMeter} position={[1.3,0,0]} >
-            <boxBufferGeometry attach="geometry" args={[0.1,1,0.1]}/>
-            <meshStandardMaterial attach="material" color="blue"/>
-        </mesh>
-        <mesh ref={cosMeter} position={[0,-1.3,0]} >
-            <boxBufferGeometry attach="geometry" args={[1,0.1,0.1]}/>
-            <meshStandardMaterial attach="material" color="red"/>
-        </mesh>
-        <mesh position={[1.4,0,0]}>
-            <textGeometry attach='geometry' args={[rotor && rotor.current ? Math.sin(rotor.current.rotation.z).toFixed(3) : "err", textOptions]} />
-            <meshStandardMaterial attach='material' />
-        </mesh>
-        <mesh position={[0,-1.5,0]}>
-            <textGeometry attach='geometry' args={[rotor && rotor.current ? Math.cos(rotor.current.rotation.z).toFixed(3) : "err", textOptions]} />
-            <meshStandardMaterial attach='material' />
-        </mesh>
-    </>
+
+            /*Sine bar. */
+            <mesh /*ref={sineMeter}*/ position={[1.3,(Math.sin(rotate) / 2),0]} scale={[1, Math.sin(rotate), 1]} >
+                <boxBufferGeometry attach="geometry" args={[0.1,1,0.1]}/>
+                <meshStandardMaterial attach="material" color="blue"/>
+            </mesh>
+
+            /*Cos bar. */
+            <mesh /*ref={cosMeter}*/ position={[(Math.cos(rotate) / 2),-1.3,0]} scale={[Math.cos(rotate), 1, 1]} >
+                <boxBufferGeometry attach="geometry" args={[1,0.1,0.1]}/>
+                <meshStandardMaterial attach="material" color="red"/>
+            </mesh>
+
+            /*Sine  3D number. */
+            <mesh position={[1.4,0,0]}>
+                <textGeometry attach='geometry' args={[Math.sin(rotate).toFixed(3), textOptions]} />
+                <meshStandardMaterial attach='material' />
+            </mesh>
+
+            /*Cos  3D number. */
+            <mesh position={[0,-1.5,0]}>
+                <textGeometry attach='geometry' args={[Math.cos(rotate).toFixed(3), textOptions]} />
+                <meshStandardMaterial attach='material' />
+            </mesh>
+        </>
     )
 }
 
