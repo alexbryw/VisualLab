313e284c757d02f4e76299a30ba22f53f4c69822
alexbryw <alexbryw@gmail.com>
2021-01-24 22:00:02 +0100

    Added new challenge file.

---
+++ challengeData/challengeData.js
@@ -0,0 +1,12 @@
+export const challengeLvl1 = [
+    {
+        question: "Find 45 degrees.",
+        answer: 45,
+        type: "deg"
+    },
+    {
+        question: "Find 90 degrees.",
+        answer: 90,
+        type: "deg"
+    },
+]
