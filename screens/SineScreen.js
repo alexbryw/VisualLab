367cfcc037136a597ec5282d52f381dcd8bbb42e
alexbryw <alexbryw@gmail.com>
2021-01-26 22:26:35 +0100

    Added win and restart states, added icons and onboarding modal.

--- screens/SineScreen.js
+++ screens/SineScreen.js
@@ -1,34 +1,88 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import { StyleSheet, View, Button, Text, Modal, TouchableHighlight } from 'react-native';
 import { Canvas } from 'react-three-fiber';
 import Plane from '../components/Plane'
 // import Box from '../components/Box'
 import UnitCircle from '../components/UnitCircle';
 import { challengeLvl1 } from '../challengeData/challengeData'
+import { Feather, FontAwesome } from '@expo/vector-icons'; 
 
 const SineScreen = () => {
-  const [rotate, setRotation] = useState(0);
+  const [rotate, setRotate] = useState(0);
+  const [modalVisible, setModalVisible] = useState(true);
+  const [wins, setWins] = useState(0)
+  const [currentQuestionPos, setCurrentQuestionPos] = useState(0)
+  const [questionVisible, setQuestionVisible] = useState(false)
+  const [gameWon, setGameWon] = useState(false);
+  const [showDegNr, setShowDegNr] = useState(true)
+  const [showStart, setShowStart] = useState(false)
 
   const moveRotor = (moveDeg) => {
-    setRotation(
+    setRotate(
       rotate + THREE.MathUtils.degToRad(moveDeg)
     )
   }
 
   const checkAnswer = () => {
-    console.log("Checking answer ", THREE.MathUtils.radToDeg(rotate).toFixed(3))
-    console.log(parseInt(THREE.MathUtils.radToDeg(rotate)))
-    console.log(challengeLvl1[0].answer)
-    // console.log(challengeLvl1)
-    if(parseInt(THREE.MathUtils.radToDeg(rotate).toFixed(0)) === challengeLvl1[0].answer) {
+    if(parseInt(THREE.MathUtils.radToDeg(rotate).toFixed(0)) === challengeLvl1[currentQuestionPos].answer) {
       console.log("Correct! You Win!")
+      setWins(wins + 1)
+      nextQuestion()
     } else {
       console.log("Wrong! You Loose!")
+      nextQuestion()
     }
+  }
+
+  const nextQuestion = () => {
+    if(currentQuestionPos + 1 < challengeLvl1.length) {
+      setCurrentQuestionPos(currentQuestionPos + 1)
+    } else {
+      setQuestionVisible(false)
+      setGameWon(true)
+      setShowDegNr(true)
+      console.log("Game Over, score ", wins)
+    }
+  }
+
+  const startChallenge = () => {
+    setShowStart(false)
+    setQuestionVisible(true)
+    setCurrentQuestionPos(0)
+    setWins(0)
+    setGameWon(false)
+    setShowDegNr(false)
+    setRotate(0)
   }
 
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          // Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Rotate whit the buttons below.</Text>
+            <Text style={styles.modalText}>When you found the correct angle press enter.</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                setShowStart(true)
+                // startChallenge()
+              }}
+            >
+              <Text style={styles.textStyle}>Got it!</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
       <Canvas /*camera={{ position: [2, 2, 2] }}*/>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
@@ -37,14 +91,63 @@ const SineScreen = () => {
         {/* <Plane /> */}
         {/* <Box position={[-1.2, 0, 0]} />
         <Box position={[boxPosition, 0, 0]} /> */}
-        <UnitCircle rotate={rotate} />
+        <UnitCircle rotate={rotate} showDegNr={showDegNr} idleAnimation={showStart || gameWon}/>
       </Canvas>
+      {showStart &&
+        <View style={styles.gameWon}>
+          {/* <Text>{wins}</Text> */}
+          {/* <Text style={styles.questionText}>
+            Game Over.
+          </Text> */}
+          <TouchableHighlight
+            style={styles.openButton}
+            onPress={() => startChallenge()}>
+            <Text>START</Text>
+          </TouchableHighlight>
+        </View>
+      }
+      {questionVisible &&
+        <View style={styles.questionHolder}>
+          <Text>{wins}</Text>
+          <Text style={styles.questionText}>
+            {(currentQuestionPos + 1).toString() + ": " + challengeLvl1[currentQuestionPos].question}
+          </Text>
+        </View>
+      }
+      {gameWon &&
+        <View style={styles.gameWon}>
+          <Text>{wins}</Text>
+          <FontAwesome name="star" size={24} color="black" />
+          {/* {for (let index = 0; index < wins; index++) {
+            // const element = array[index];
+            <FontAwesome name="star" size={24} color="black" />
+            
+          }} */}
+          <Text style={styles.questionText}>
+            Game Over.
+          </Text>
+          <TouchableHighlight
+            style={styles.openButton}
+            onPress={() => {
+              setGameWon(false)
+              setShowStart(true)
+            }}>
+            <Text>OK</Text>
+          </TouchableHighlight>
+        </View>
+      }
       <View style={styles.controllerContainer}>
         <View style={styles.buttonHolder}>
-          <Button title="Up" onPress={() => moveRotor(5)} />
-          <Text>{parseInt(THREE.MathUtils.radToDeg(rotate).toFixed(0))}°</Text>
-          <Button title="Down" onPress={() => moveRotor(-5)} />
-          <Button title="Enter" onPress={() => checkAnswer()}/>
+          <TouchableHighlight onPress={() => moveRotor(5)}>
+            <Feather name="rotate-ccw" size={42} color="black" />
+          </TouchableHighlight>
+          {/* <Button title="Up" onPress={() => moveRotor(5)} /> */}
+          {/* <Text>{parseInt(THREE.MathUtils.radToDeg(rotate).toFixed(0))}°</Text> */}
+          {/* <Button title="Down" onPress={() => moveRotor(-5)} /> */}
+          <Button disabled={showStart || gameWon || currentQuestionPos === 0 && rotate == 0} title="Enter" onPress={() => checkAnswer()}/>
+          <TouchableHighlight onPress={() => moveRotor(-5)}>
+            <Feather name="rotate-cw" size={42} color="black" />
+          </TouchableHighlight>
         </View>
       </View>
     </View>
@@ -54,7 +157,7 @@ const SineScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'grey',
   },
   controllerContainer: {
     flex: 1,
@@ -66,9 +169,71 @@ const styles = StyleSheet.create({
     // backgroundColor: "green",
     bottom: 0
   },
+  questionHolder: {
+    position: "absolute",
+    marginTop: 42,
+    // top: 0,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center"
+
+  },
+  questionText: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingHorizontal: 10,
+  },
+  gameWon: {
+    position: "absolute",
+    marginTop: 42,
+    // top: 0,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center"
+  },
   buttonHolder: {
-    flexDirection: "row"
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginTop: 22,
+    marginBottom: 64,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
+
 });
 
 export default SineScreen
