254171efd0f7e84f7561c2dfebcbab244c189fb1
alexbryw <alexbryw@gmail.com>
2021-01-28 19:31:15 +0100

    Added title and new color to overview.

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -2,25 +2,30 @@ import React from 'react'
 import { StyleSheet, Text, View, Pressable, ScrollView, SafeAreaView} from 'react-native'
 import CategoryCard from '../components/CategoryCard'
 const water1 = require('../media/water1.jpg')
+const rocket = require('../media/space.jpg')
 
 const OverviewScreen = ({ navigation }) => {
+    const OverviewText = [
+        {
+            title: "Waves",
+            text: "All about waves! Learn about circles, degrees, sine, cosine and interference patterns."
+         }
+    ]
     return (
         <SafeAreaView style={styles.container}>
         <ScrollView contentContainerStyle={styles.scrollView}>
-            <CategoryCard navigation={navigation} navTo={"Wave"} imgFile={water1}/>
-            <CategoryCard navigation={navigation} />
-
-            {/* <View style={styles.placeholderBox}>
-                <Text>Waves. Image coming soon.</Text>
+            <View style={styles.headerTextHolder}>
+                <Text style={styles.headerTextTitle}>CATEGORIES</Text>
             </View>
-            <Text>Learn about the Unit Circle.</Text>
-            <Pressable style={styles.nextButton}>
-                <Text onPress={() => navigation.navigate('Wave')}>Wave</Text>
-            </Pressable>
-
-            <View style={styles.placeholderBox}>
-                <Text>Rockets. Image coming soon.</Text>
-            </View> */}
+            <CategoryCard
+                navigation={navigation}
+                navTo={"Wave"}
+                imgFile={water1}
+                textTitle={OverviewText[0].title}
+                cardText={OverviewText[0].text}
+            />
+            <CategoryCard navigation={navigation} textTitle="Propulsion" cardText="Coming soon..." imgFile={rocket}/>
+            <CategoryCard navigation={navigation} />
         </ScrollView>
         </SafeAreaView>
     )
@@ -36,6 +41,7 @@ const styles = StyleSheet.create({
         // backgroundColor: "white"
     },
     scrollView: {
+        paddingTop: 48,
         alignItems: "center",
         justifyContent: "flex-start",
         backgroundColor: "white",
@@ -63,5 +69,14 @@ const styles = StyleSheet.create({
         // flex: 1,
         bottom: 0,
         // width: "100%",
+    },
+    headerTextHolder: {
+
+    },
+    headerTextTitle: {
+        fontSize: 24,
+        color: '#00C4EE',
+        letterSpacing: 1.6,
+        fontWeight: "bold",
     },
 })
